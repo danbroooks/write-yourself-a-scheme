cc0347a4ab21c0035b8f82646b690cefb1ae6267
Dan Brooks <dmbrooks@live.co.uk>
2018-02-26 17:38:58 +0000

    Complete tasks from first chapter

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,17 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn $ "Hello, " ++ head args
+  case args of
+      "greet" : name : _ ->
+        putStrLn $ "Hello, " ++ name
+
+      "sum_two" : a : b : _ ->
+        putStrLn $ "Result: " ++ show (read a + read b)
+
+      "ask" : _ -> do
+        putStrLn "What is your name?"
+        name <- getLine
+        putStrLn $ "Hello, " ++ name
+
+      _ ->
+        putStrLn "Unknown command"
